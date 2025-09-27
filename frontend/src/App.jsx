6f304ae8bc1c639a54ec6b38fdb16864366675fd
NAROA GONZALEZ OCON <naroa.gonzalez@caf.net>
2025-09-27 11:29:19 +0200

    Usar hash router en vez de browse router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import FormTemplate from "./FormTemplate";
 import ExcelUploader from "./ExcelUploader";
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 
-//const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 
-const API_URL = "http://127.0.0.1:8000";
+//const API_URL = "http://127.0.0.1:8000";
 console.log("URL del backend:", import.meta.env.VITE_API_URL);
 
 
